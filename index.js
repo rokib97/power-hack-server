34bba687b5d3387ee0b38fab433b466df9156927
rokib97 <hasanrokib1997@gmail.com>
2022-06-30 17:32:59 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello From Power Hack!");
+});
+
+app.listen(port, () => {
+  console.log(`Power Hack app listening on port ${port}`);
+});
